8b33018f170a572a1642e7866a0a7339226da4c3
FSY <fesyaazzahraa@gmail.com>
2026-04-15 09:00:57 +0700

    version 2
    Updated variable names and description for clarity.

--- soal2.c
+++ soal2.c
@@ -3,29 +3,33 @@
  *   Hari dan Tanggal    : Rabu, 15 April 2026
  *   Nama (NIM)          : Fesya Dea Azzahra (13224049)
  *   Nama File           : soal2.c
- *   Deskripsi           : Menghitung banyak langkah sampai menjadi 1 dengan aturan transformasi tertentu
+ *   Deskripsi           : Menghitung banyak langkah (transformasi) hingga n menjadi 1 dengan aturan transformasi tertentu
  * 
  */
+// Soal 2: Kode Batu Stabil
 
 #include <stdio.h>
 
 void sumOfSteps (int n) {
-    int steps = 0;
+    int langkah = 0;
     while (n != 1) {
         if (n % 2 == 0) {
             n /= 2; // Jika n genap, bagi dengan 2
+            langkah++;
         } else {
             n = 3 * n + 1; // Jika n ganjil, kalikan dengan 3 dan tambahkan 1
+            langkah++;
         }
-        steps++; // Hitung langkah yang diperlukan
+        return 0; 
     }
-    printf("%d\n", steps); // Tampilkan jumlah langkah yang diperlukan untuk mencapai 1
+    printf("%d\n", langkah); // Tampilkan jumlah langkah yang diperlukan untuk mencapai 1
 }
 
 int main () {
     int N;
-    printf("Masukkan N: ");
+    printf("%d\n", N);
     scanf("%d", &N);
     sumOfSteps(N);
     return 0;
 }
+
